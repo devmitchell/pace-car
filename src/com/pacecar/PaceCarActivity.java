6b3bf85579a23b44499b8c66c1ceeea021d6b913
Devin Mitchell <devmitchell@gmail.com>
2012-06-08 20:04:34 -0500

    Formatted missed java files.

--- src/com/pacecar/PaceCarActivity.java
+++ src/com/pacecar/PaceCarActivity.java
@@ -4,10 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class PaceCarActivity extends Activity {
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-	}
+  
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+  
 }
